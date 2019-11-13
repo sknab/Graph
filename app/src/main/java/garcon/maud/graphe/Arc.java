1f694168c707b5c23cb32bdbe1b6e40beedca1cd
sknab <35455366+sknab@users.noreply.github.com>
2019-11-13 14:40:06 +0100

    correction commentaires

--- app/src/main/java/garcon/maud/graphe/Arc.java
+++ app/src/main/java/garcon/maud/graphe/Arc.java
@@ -4,13 +4,13 @@ import android.graphics.Path;
 import android.graphics.Paint;
 
 /**
- * Created by Maud Garçon & Saly Knab on 10/10/2019.
+ * Created by Maud Garçon & Saly Knab
  */
 
 public class Arc {
-    // Noeud de départ de l'arc
+    // Noeud de depart de l'arc
     private Node noeudDepart;
-    // Noeud d'arrivé de l'arc
+    // Noeud d'arrive de l'arc
     private Node noeudArrive;
     //Nom de l'arc
     private String nom;
@@ -20,13 +20,13 @@ public class Arc {
 
     //constructeur de l'arc
     public Arc(Node noeudDepart, Node noeudArrive, String nom) {
-        //definition du noeud de départ et d'arrivé
+        //definition du noeud de depart et d'arrive
         this.noeudDepart = noeudDepart;
         this.noeudArrive = noeudArrive;
         //definition du nom de l'arc
         this.nom = nom;
 
-        //on doit definir une taille et un style dans le paint pour le l'arc soit visible
+        //on doit definir une taille et un style dans le paint pour que l'arc soit visible
         paint.setStrokeWidth(6F);
         paint.setStyle(Paint.Style.STROKE);
     }
